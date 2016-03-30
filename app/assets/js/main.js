fd74f6487668104f283b50f34089dd1d1ab494d3
diwanee <diwanee@Mac-Lutalica.local>
2016-03-30 17:24:21 +0200

    mobile, tablet

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,6 +1,27 @@
+// Scroll to section
 function scrollTo(sectionId, speed){
   var offset = 50;
   $('html, body').animate({
       scrollTop: $(sectionId).offset().top + offset
   }, speed);
 }
+// On scroll header__container background opacity
+var scroll;
+$(function(){
+  $(window).scroll(function(){
+    scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll>400){
+      $(".header__container").css({"background":"rgba(255,255,255,1)"},400);
+    } else {
+      var opacity = scroll/300;
+      $(".header__container").css({"background":'rgba(255,255,255,'+opacity+')'},400);
+    }
+  });
+});
+// Nav menu functionality
+$(function(){
+  $(".menu-ico").click(function(){
+    $(".nav").toggleClass("active");
+  });
+});
